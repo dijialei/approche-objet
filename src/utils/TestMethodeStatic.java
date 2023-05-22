7fe8fab0884feec1062771042b08e4656d5390e0
dijialei <dijialei0605@gmail.com>
2023-05-22 10:32:42 +0200

    tp4 - fini

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.lang.Integer;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int intObj = Integer.parseInt(chaine);
+        int a = 1;
+        int b = 2;
+        int max = Integer.max(a, b);
+
+    }
+}
